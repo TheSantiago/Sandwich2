e4454de7655610f8d3ae3e6c4358b5fa09e49b54
Martux99 <34321961+Martux99@users.noreply.github.com>
2018-05-01 18:50:49 -0500

    Maxvel
    as

---
+++ Project Snitch/Assets/Velocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour {
+    public Rigidbody2D lechuga;
+    public int maxvel;
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if (lechuga.velocity.y < maxvel)
+        {
+            Debug.Log("Lechuga asesina");
+            lechuga.velocity = new Vector2(lechuga.velocity.x, maxvel);
+        }
+	}
+}
